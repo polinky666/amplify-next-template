6fa0289984996cbbc3ab7cb631701e183ec6f26c
polinky666 <polinky666@gmail.com>
2024-12-15 19:23:21 +0900

    test-1215-4

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-//import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import { useAuthenticator } from "@aws-amplify/ui-react";
+import { useAuthenticator, DropZone, Text  } from "@aws-amplify/ui-react";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
@@ -25,6 +25,7 @@ const client = generateClient<Schema>();
 
 export default function App() {
   // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [files, setFiles] = useState<Array<File>>([]);
   const { user, signOut } = useAuthenticator();
   // function listTodos() {
   //   client.models.Todo.observeQuery().subscribe({
@@ -52,46 +53,51 @@ export default function App() {
   // }
 
   function uploadFile(){
-    const fileInput: HTMLInputElement | null = document.getElementById('fileInput') as HTMLInputElement;
-    const file = fileInput?.files?.[0];
+    // const fileInput: HTMLInputElement | null = document.getElementById('fileInput') as HTMLInputElement;
+    // const file = fileInput?.files?.[0];
 
-    if (!file) {
-        const statusElem = document.getElementById('status');
-        statusElem!.innerText = 'Please select a file.';
-        return;
-    }
-
-    try {
-      const result = uploadData({
-        key: file.name,
-        data: file,
-        options:{ 
-      // Alternatively, provide bucket name from console and associated region
-          bucket: {
-            bucketName: 'hotcake-test-bucket-1118',
-            region: 'ap-northeast-1'
-          }
-        }
-      }).result;
-
-      console.log("Upload successful:", result);
-      document.getElementById("status")!.innerText = "File uploaded";
-    } catch (error) {
-      console.error("Upload failed:", error);
-      document.getElementById("status")!.innerText = "Upload failed.";
-    }
+    // if (!file) {
+    //     const statusElem = document.getElementById('status');
+    //     statusElem!.innerText = 'Please select a file.';
+    //     return;
+    // }
+    for (const file of files) {  
+      try {
+          const result = uploadData({
+            key: files[0].name,
+            data: files[0],
+            options:{ 
+          // Alternatively, provide bucket name from console and associated region
+              bucket: {
+                bucketName: 'hotcake-test-bucket-1118',
+                region: 'ap-northeast-1'
+              }
+            }
+          }).result;
+          console.log("Upload success:", result);    
+        }catch (error) {
+          console.error("Upload error:", error);
+        } 
+    };
+    document.getElementById("status")!.innerText = "File uploaded";
   }
 
   return (
     <main>
       <h1>user:{user?.signInDetails?.loginId}</h1>
       <div>
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
+        <DropZone
+        onDropComplete={({ acceptedFiles, rejectedFiles }) => {
+          setFiles(acceptedFiles);
+        }}
+      >
+        Drag file here
+      </DropZone>
+      {files.map((file) => (
+        <Text key={file.name}>{file.name}</Text>
+      ))}
       </div>
-      <input type="file" id="fileInput" />
+      {/* <input type="file" id="fileInput" /> */}
       <button id="uploadButton" onClick={uploadFile}>Upload</button>
       <p id="status"></p>
       
